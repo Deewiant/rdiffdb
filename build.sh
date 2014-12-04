22271df68e8e592c82d78955c9d2718778fe50ea
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2014-12-04 14:49:02 +0200

    build.sh: build and link in the MARISA bindings

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh
 set -o errexit
-clang -ordiffdb rdiffdb.c -lleveldb $(pkg-config --cflags --libs glib-2.0) -std=c11 -pedantic -Weverything -Wno-{cast-align,disabled-macro-expansion,documentation{,-unknown-command},sign-{conversion,compare}} -O3 -flto -march=native -fuse-ld=gold
+clang++ -c -O3 -flto -march=native marisa.cc -std=c++14 -pedantic -Weverything
+clang -ordiffdb rdiffdb.c marisa.o -lleveldb -lmarisa -lstdc++ $(pkg-config --cflags --libs glib-2.0) -std=c11 -pedantic -Weverything -Wno-{cast-align,disabled-macro-expansion,documentation{,-unknown-command},sign-{conversion,compare}} -O3 -flto -march=native -fuse-ld=gold
